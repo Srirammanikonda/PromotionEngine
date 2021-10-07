27ecb74c4cbc5d114cbdeab1e9cb8243c32ee195
Sriram Manikonda <Sriram.Manikonda@finastra.com>
2021-10-07 18:47:15 +0530

    Initial Commit

---
+++ src/main/java/com/PromEngine/PromotionEngineImplApplication.java
@@ -0,0 +1,13 @@
+package com.PromEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionEngineImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionEngineImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PromEngine/PromotionEngineImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PromEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
